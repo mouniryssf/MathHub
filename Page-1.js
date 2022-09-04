370cc49b0dcf55aeae09810b88bb2aa1273f3b02
mouniryssf <59393219+mouniryssf@users.noreply.github.com>
2022-09-04 20:10:10 +0200

    Add files via upload

--- Page-1.js
+++ Page-1.js
@@ -1,1 +1,24 @@
 
+  let assignmentDescription = document.querySelector("#assignmentDescription")
+  let numNeededDesc = assignmentDescriptionArr.length
+
+  let assignmentWindow = document.querySelector("#assignmentWindow")
+
+  let feedbackForm = document.querySelector("#feedbackform")
+
+  let template = assignmentDescription.children[0]
+
+
+
+  assignmentDescription.replaceChildren()
+
+  for (let i = 0; i < numNeededDesc; i++) {
+    let node = template.cloneNode(true)
+    let text = node.childNodes[2]
+    text.textContent = assignmentDescriptionArr[i]
+    assignmentDescription.appendChild(node)
+  }
+
+  
+    let window = assignmentWindow.getElementsByTagName("iframe")[0].src = assignmentLink
+    let form = feedbackForm.getElementsByTagName("iframe")[0].src = formLink
--- Page-4.js
+++ Page-4.js
@@ -1,44 +1,3 @@
-  // Input Lesson Details
-//   const objectivesArr = [
-//     'Identify "Like Terms" in an expression',
-//     'Add & Subtract "Like Terms"',
-//     'Write expressions in "Simplest Form"'
-//   ]
-
-//   const vocabularyArr = [
-//     'Collect / Combine',
-//     'Like Terms',
-//     'Expressions',
-//     'Coefficients',
-//     'Variables',
-//     'Constants',
-//     'Terms (Associated Signs)'
-//   ]
-
-//   const priorKnowledgeArr = [
-//     "Prior 1",
-//     "Prior 2",
-//     "Prior 3",
-//     "Prior 4"
-//   ]
-
-//   const applicationArr = [
-//     "Lesson 1: List the steps for combining like terms. In your notes, highlight the following terms: variables, like terms, unlike terms, cubed variable.",
-//   ]
-
-//   const videosArr = [
-//     { title: "Black Panther", url: "https://www.youtube.com/watch?v=RlOB3UALvrQ" },
-//     { title: "Moon Knight", url: "https://www.youtube.com/watch?v=x7Krla_UxRg" },
-//     { title: "Mounir's Video", url: "uploaded" },
-//     { title: "Avengers : Infinity War", url: "https://www.youtube.com/watch?v=6ZfuNTqbHE8" },
-//     { title: "Mounir's Second Video", url: "uploaded" },
-//     { title: "Thor : Love & Thunder", url: "https://www.youtube.com/watch?v=Go8nTmfrQd8" },
-//     { title: "Doctor Strange in The Multiverse of Madness", url: "https://www.youtube.com/watch?v=aWzlQ2N6qqg" },
-//     { title: "Century Videos", url: "//app.century.tech/learn/my-path/study-groups/e5637f15-6948-4a11-8550-567cbd2d0dd5/nuggets/4adaffb7-84de-462d-896a-6016ca9e79dc" }
-//   ]
-
-
-  // Adjust Lesson Using Details
 
   let objectivesList = document.querySelector("#objectives")
   let numNeededObj = objectivesArr.length
@@ -94,7 +53,10 @@
 
 
 
-  // Handle Videos
+
+
+
+
   let videosList = document.querySelector("#videosDiv")
   let videoTemplate = videosList.children[0]
   let numNeededVids = videosArr.length
